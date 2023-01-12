7d742fb893bec1be4748405fb2e3ecc31e68d6f5
Rakesh Kumar <acousticrakesh0827@gmail.com>
2023-01-12 23:55:01 +0530

    Quick Solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -10,22 +10,4 @@ public class Main {
     }
 }
 
-class Product{
-
-    Product(){
-
-    }
-
-    public int product(int x, int y){
-        return x*y;
-    }
-
-    public int product(int x, int y, int z){
-        return x*y*z;
-    }
-
-    public double product(double x, double y){
-        return x*y;
-    }
-}
 
