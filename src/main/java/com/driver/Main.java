72118eb2186708de853691c52a200b73e49f8210
Rakesh Kumar <acousticrakesh0827@gmail.com>
2023-01-13 12:26:09 +0530

    Quick Commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -10,4 +10,22 @@ public class Main {
     }
 }
 
+class Product{
+
+    Product(){
+
+    }
+
+    public int product(int x, int y){
+        return x*y;
+    }
+
+    public int product(int x, int y, int z){
+        return x*y*z;
+    }
+
+    public double product(double x, double y){
+        return x*y;
+    }
+}
 
